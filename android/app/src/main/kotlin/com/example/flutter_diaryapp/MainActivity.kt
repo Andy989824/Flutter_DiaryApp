af60e45ea327a32fb9ed5907cd139d7cf3bdd09b
Andy989824 <andylau989824@gmail.com>
2021-07-10 21:21:19 +0800

    first commit and adding files

---
+++ android/app/src/main/kotlin/com/example/flutter_diaryapp/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_diaryapp
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Notes.dart
@@ -0,0 +1,488 @@
+import 'dart:ui';
+import 'package:intl/intl.dart';
+import 'package:auto_size_text/auto_size_text.dart';
+import 'package:flutter/material.dart';
+import 'Models/NotesPage.dart';
+
+class NotesPage extends StatefulWidget {
+  NotesPage({Key key}) : super(key: key);
+
+  @override
+  _NotesPageState createState() => _NotesPageState();
+}
+
+class _NotesPageState extends State<NotesPage> {
+  var _formKey = GlobalKey<FormState>();
+  var now = new DateTime.now();
+  var formatter = new DateFormat('yyyy-MM-dd');
+  @override
+  void initState() {
+    super.initState();
+    notesDescriptionMaxLenth =
+        notesDescriptionMaxLines * notesDescriptionMaxLines;
+  }
+
+  @override
+  void dispose() {
+    noteDescriptionController.dispose();
+    noteHeadingController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: notesHeader(),
+      ),
+      body: noteHeading.length > 0
+          ? buildNotes()
+          : Center(child: Text("Add a New Diary...")),
+      floatingActionButton: FloatingActionButton(
+        mini: false,
+        backgroundColor: Colors.blueAccent,
+        onPressed: () {
+          _settingModalBottomSheet(context);
+        },
+        child: Icon(Icons.create),
+      ),
+    );
+  }
+
+  Widget buildNotes() {
+    return Padding(
+      padding: const EdgeInsets.only(
+        top: 10,
+        left: 10,
+        right: 10,
+      ),
+      child: new ListView.builder(
+        itemCount: noteHeading.length,
+        itemBuilder: (context, int index) {
+          return Padding(
+            padding: const EdgeInsets.only(bottom: 5.5),
+            child: new Dismissible(
+              key: UniqueKey(),
+              direction: DismissDirection.horizontal,
+              onDismissed: (direction) {
+                setState(() {
+                  deletedNoteHeading = noteHeading[index];
+                  deletedNoteDescription = noteDescription[index];
+                  noteHeading.removeAt(index);
+                  noteDescription.removeAt(index);
+                  Scaffold.of(context).showSnackBar(
+                    new SnackBar(
+                      backgroundColor: Colors.purple,
+                      content: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          new Text(
+                            "Diary Deleted",
+                            style: TextStyle(),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                });
+              },
+              background: ClipRRect(
+                borderRadius: BorderRadius.circular(5.5),
+                child: Container(
+                  color: Colors.red,
+                  child: Align(
+                    alignment: Alignment.centerRight,
+                    child: Padding(
+                      padding: const EdgeInsets.only(right: 10),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.delete,
+                            color: Colors.white,
+                          ),
+                          Text(
+                            "Delete",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+              child: noteList(index),
+            ),
+          );
+        },
+      ),
+    );
+  }
+
+  Widget noteList(int index) {
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(5.5),
+      child: Container(
+        width: double.infinity,
+        decoration: BoxDecoration(
+          color: noteColor[(index % noteColor.length).floor()],
+          borderRadius: BorderRadius.circular(5.5),
+        ),
+        height: 150,
+        child: Center(
+          child: Row(
+            children: [
+              new Container(
+                color:
+                    noteMarginColor[(index % noteMarginColor.length).floor()],
+                width: 3.5,
+                height: double.infinity,
+              ),
+              Flexible(
+                child: Padding(
+                  padding: const EdgeInsets.only(left: 10, right: 10, top: 10),
+                  child: new Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Flexible(
+                        child: Text(
+                          date[index],
+                          overflow: TextOverflow.ellipsis,
+                          style: TextStyle(
+                            fontSize: 20.00,
+                            color: Colors.black,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                      SizedBox(
+                        height: 2.5,
+                      ),
+                      Flexible(
+                        child: Container(
+                          height: double.infinity,
+                          child: AutoSizeText(
+                            "${(noteHeading[index])}",
+                            maxLines: 2,
+                            overflow: TextOverflow.ellipsis,
+                            style: TextStyle(
+                              fontSize: 15.00,
+                              color: Colors.black,
+                            ),
+                          ),
+                        ),
+                      ),
+                      GestureDetector(
+                        child: Container(
+                          child: Row(
+                            children: [
+                              Text(
+                                "View",
+                                style: TextStyle(
+                                  fontSize: 20.00,
+                                  color: Colors.blue,
+                                  fontWeight: FontWeight.w500,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        onTap: () {
+                          _showDiaryDetails(context, index);
+                        },
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+
+  void _showDiaryDetails(context, index) {
+    showModalBottomSheet(
+      context: context,
+      isScrollControlled: true, // full height on screen
+      elevation: 50, // z-index
+      isDismissible: true,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(
+          topRight: Radius.circular(30),
+          topLeft: Radius.circular(30),
+        ),
+      ),
+      clipBehavior: Clip.antiAliasWithSaveLayer,
+      builder: (BuildContext bc) {
+        return Padding(
+          padding: EdgeInsets.only(left: 25, right: 25),
+          child: Form(
+            key: _formKey,
+            child: SingleChildScrollView(
+              child: ConstrainedBox(
+                constraints: BoxConstraints(
+                  minHeight: (MediaQuery.of(context).size.height),
+                ),
+                child: Padding(
+                  padding: EdgeInsets.only(bottom: 250, top: 50),
+                  child: new Column(
+                    children: <Widget>[
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "${(noteHeading[index])}",
+                            style: TextStyle(
+                              fontSize: 20.00,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                          GestureDetector(
+                            child: Container(
+                              child: Row(
+                                children: [
+                                  Text(
+                                    "Save",
+                                    style: TextStyle(
+                                      fontSize: 20.00,
+                                      color: Colors.blue,
+                                      fontWeight: FontWeight.w500,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            onTap: () {
+                              if (_formKey.currentState.validate()) {
+                                setState(() {
+                                  noteDescription[index] =
+                                      (noteDescriptionController.text);
+                                  noteDescriptionController.clear();
+                                });
+                                Navigator.pop(context);
+                              }
+                            },
+                          ),
+                        ],
+                      ),
+                      Divider(
+                        color: Colors.blueAccent,
+                        thickness: 2.5,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 15),
+                        child: Container(
+                          margin: EdgeInsets.all(1),
+                          height: 5 * 24.0,
+                          child: TextFormField(
+                            focusNode: textSecondFocusNode,
+                            maxLines: notesDescriptionMaxLines,
+                            maxLength: notesDescriptionMaxLenth,
+                            controller: noteDescriptionController,
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              hintText: noteDescription[index],
+                              hintStyle: TextStyle(
+                                fontSize: 15.00,
+                                color: Colors.black,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  void _settingModalBottomSheet(context) {
+    showModalBottomSheet(
+      context: context,
+      isScrollControlled: true, // full height on screen
+      elevation: 50, // z-index
+      isDismissible: true,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.only(
+          topRight: Radius.circular(30),
+          topLeft: Radius.circular(30),
+        ),
+      ),
+      clipBehavior: Clip.antiAliasWithSaveLayer,
+      builder: (BuildContext bc) {
+        return Padding(
+          padding: EdgeInsets.only(left: 25, right: 25),
+          child: Form(
+            key: _formKey,
+            child: SingleChildScrollView(
+              child: ConstrainedBox(
+                constraints: BoxConstraints(
+                  minHeight: (MediaQuery.of(context).size.height),
+                ),
+                child: Padding(
+                  padding: EdgeInsets.only(bottom: 250, top: 50),
+                  child: new Column(
+                    children: <Widget>[
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: [
+                          Text(
+                            "New Diary",
+                            style: TextStyle(
+                              fontSize: 20.00,
+                              fontWeight: FontWeight.w500,
+                            ),
+                          ),
+                          GestureDetector(
+                            child: Container(
+                              child: Row(
+                                children: [
+                                  Text(
+                                    "Save",
+                                    style: TextStyle(
+                                      fontSize: 20.00,
+                                      color: Colors.blue,
+                                      fontWeight: FontWeight.w500,
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ),
+                            onTap: () {
+                              if (_formKey.currentState.validate()) {
+                                setState(() {
+                                  date.add(formatter.format(now));
+                                  noteHeading.add(noteHeadingController.text);
+                                  noteDescription
+                                      .add(noteDescriptionController.text);
+                                  noteHeadingController.clear();
+                                  noteDescriptionController.clear();
+                                });
+                                Navigator.pop(context);
+                              }
+                            },
+                          ),
+                        ],
+                      ),
+                      Divider(
+                        color: Colors.blueAccent,
+                        thickness: 2.5,
+                      ),
+                      TextFormField(
+                        maxLength: notesHeaderMaxLenth,
+                        controller: noteHeadingController,
+                        decoration: InputDecoration(
+                          hintText: "Heading of your Diary...",
+                          hintStyle: TextStyle(
+                            fontSize: 15.00,
+                            color: Colors.grey,
+                            fontWeight: FontWeight.w500,
+                          ),
+                          prefixIcon: Icon(Icons.text_fields),
+                        ),
+                        validator: (String noteHeading) {
+                          if (noteHeading.isEmpty) {
+                            return "Please enter Note Heading";
+                          } else if (noteHeading.startsWith(" ")) {
+                            return "Please avoid whitespaces";
+                          }
+                        },
+                        onFieldSubmitted: (String value) {
+                          FocusScope.of(context)
+                              .requestFocus(textSecondFocusNode);
+                        },
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 15),
+                        child: Container(
+                          margin: EdgeInsets.all(1),
+                          height: 5 * 24.0,
+                          child: TextFormField(
+                            focusNode: textSecondFocusNode,
+                            maxLines: notesDescriptionMaxLines,
+                            maxLength: 200,
+                            controller: noteDescriptionController,
+                            decoration: InputDecoration(
+                              border: OutlineInputBorder(),
+                              hintText: 'Description',
+                              hintStyle: TextStyle(
+                                fontSize: 15.00,
+                                color: Colors.grey,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                            validator: (String noteDescription) {
+                              if (noteDescription.isEmpty) {
+                                return "Please enter Note Desc";
+                              } else if (noteDescription.startsWith(" ")) {
+                                return "Please avoid whitespaces";
+                              }
+                            },
+                          ),
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ),
+        );
+      },
+    );
+  }
+}
+
+Widget notesHeader() {
+  return Padding(
+    padding: const EdgeInsets.only(
+      top: 10,
+      left: 2.5,
+      right: 2.5,
+    ),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Container(
+          height: 50.0,
+          width: 50.0,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage('tom.jpg'),
+            ),
+            borderRadius: BorderRadius.circular(
+              10.0,
+            ),
+          ),
+        ),
+        SizedBox(
+          width: 20.0,
+        ),
+        Text(
+          "My Diary",
+          style: TextStyle(
+            color: Colors.blueAccent,
+            fontSize: 25.00,
+            fontWeight: FontWeight.w500,
+          ),
+        ),
+        Divider(
+          color: Colors.blueAccent,
+          thickness: 2.5,
+        ),
+      ],
+    ),
+  );
+}
